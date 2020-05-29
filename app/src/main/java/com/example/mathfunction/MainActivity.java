eda0ea594d140de4900e34e1e12bcc1482979c2d
aby78 <wabhi95@gmail.com>
2020-05-29 17:17:59 -0400

    app test

--- app/src/main/java/com/example/mathfunction/MainActivity.java
+++ app/src/main/java/com/example/mathfunction/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+// app test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
