0b6e7f071e76612d7386ab290766cbcb17d7ad87
aby78 <wabhi95@gmail.com>
2020-05-29 17:14:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mathfunction/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mathfunction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btndiv = (Button) findViewById(R.id.buttondiv);
+        Button btnmul = (Button) findViewById(R.id.buttonmul);
+        final EditText etv = (EditText) findViewById(R.id.editText1);
+        final EditText etv2 = (EditText) findViewById(R.id.editText2);
+        final TextView result = (TextView) findViewById(R.id.textView1);
+        // Create button click event
+
+
+
+
+        btndiv.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int x = new Integer(etv.getText().toString());
+                int y = new Integer(etv2.getText().toString());
+                int div = x / y; //Perform Maths operation
+                result.setText("The ANS of " + x + " / " + y + " = " + div);//print answer
+            }
+        });
+
+        btnmul.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int x = new Integer(etv.getText().toString());
+                int y = new Integer(etv2.getText().toString());
+                int mul = x * y; //Perform Maths operation
+                result.setText("The ANS of " + x + " * " + y + " = " + mul);//Print answer
+            }
+        });
+    }
+
+}
